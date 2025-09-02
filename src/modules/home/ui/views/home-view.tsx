03a50805c082dd9c058a6fb0fec49838d1e0ca2c
Jhampier Tucno <yantucnoperez@gmail.com>
2025-09-01 22:11:10 -0500

    feat: add agents management with error and loading states, integrate nanoid for unique IDs

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,15 +1,5 @@
 'use client';
 
-import { useTRPC } from '@/trpc/client';
-import { useQuery } from '@tanstack/react-query';
-
 export const HomeView = () => {
-  const trpc = useTRPC();
-  const data = useQuery(trpc.hello.queryOptions({ text: 'from tRPC' }));
-
-  return (
-    <div className="flex flex-col items-center justify-center h-screen space-y-6">
-      {data.data?.greeting}
-    </div>
-  );
+  return <div>Home View</div>;
 };
